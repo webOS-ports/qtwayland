264d98c2d1d1a9c70226a4402a97d13f647f97ac
Christophe Chapuis <chris.chapuis@gmail.com>
2015-07-14 12:18:14 +0200

    Ensure that the view has not yet been deleted before destroying it in the shell surface
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

--- src/compositor/wayland_wrapper/qwlshellsurface.cpp
+++ src/compositor/wayland_wrapper/qwlshellsurface.cpp
@@ -106,7 +106,10 @@ ShellSurface::ShellSurface(Shell *shell, wl_client *client, uint32_t id, Surface
 
 ShellSurface::~ShellSurface()
 {
-    delete m_view;
+   QList<QWaylandSurfaceView *> surfaceViews = m_surface->waylandSurface()->views();
+   if( surfaceViews.indexOf(m_view) >= 0 ) {
+       delete m_view;
+   }
 }
 
 void ShellSurface::sendConfigure(uint32_t edges, int32_t width, int32_t height)
